6091eb0f6e8b8e6c39de7befcd6b250fab1e0ce0
Rakhimov Emil <sinbad116@gmail.com>
2021-03-13 00:32:24 +0300

    updated tests

--- smoke_test.cpp
+++ smoke_test.cpp
@@ -146,6 +146,27 @@ void check_const() {
     assert(n3.to_string() == "10");
 }
 
+void check_constructor()
+{
+    std::string str{"1" + std::string(150, '0')};
+    mp::bignum a(str);
+    assert(a.to_string() == str);
+}
+
+void check_long_operators()
+{
+    mp::bignum a("1" + std::string(50, '0'));
+    mp::bignum b("1" + std::string(77, '0'));
+
+    std::string const aplusb("1" + std::string(26, '0') + "1" + std::string(50, '0'));
+    std::string const amultb("1" + std::string(127, '0'));
+
+    assert((a + b).to_string() == aplusb);
+    assert((a * b).to_string() == amultb);
+    assert((b + a).to_string() == aplusb);
+    assert((b * a).to_string() == amultb);
+}
+
 int main() {
     check_constructors();
     check_assignment();
@@ -154,4 +175,6 @@ int main() {
     check_io();
     check_operators();
     check_const();
+    check_constructor();
+    check_long_operators();
 }
